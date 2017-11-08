c8f1e5cd5a7ce9ae678a6e9811b507fd2f0c42af
Ioana-Sofia Pascu <ioana.pascu9596@yahoo.com>
2017-11-08 21:51:17 +0200

    Hide keyboard when tapping outside of it except when tapping inside an EditText.

--- app/src/main/java/com/ioanap/classbook/BaseActivity.java
+++ app/src/main/java/com/ioanap/classbook/BaseActivity.java
@@ -4,11 +4,16 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -495,7 +500,6 @@ public class BaseActivity extends AppCompatActivity implements GoogleApiClient.O
                 });
     }
 
-
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
@@ -514,4 +518,53 @@ public class BaseActivity extends AppCompatActivity implements GoogleApiClient.O
         firebaseAuthWithGoogle(true);
 
     }
+
+    // ========================= Keyboard ===========================
+
+    protected Rect getLocationOnScreen(EditText mEditText) {
+        Rect mRect = new Rect();
+        int[] location = new int[2];
+
+        mEditText.getLocationOnScreen(location);
+
+        mRect.left = location[0];
+        mRect.top = location[1];
+        mRect.right = location[0] + mEditText.getWidth();
+        mRect.bottom = location[1] + mEditText.getHeight();
+
+        return mRect;
+    }
+
+    /**
+     * Hide keyboard when clicking outside of it (except from clicking inside an EditText)
+     *
+     * @param ev
+     * @return
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+
+        boolean handleReturn = super.dispatchTouchEvent(ev);
+
+        View view = getCurrentFocus();
+
+        int x = (int) ev.getX();
+        int y = (int) ev.getY();
+
+        if(view instanceof EditText){
+            View innerView = getCurrentFocus();
+
+            if (ev.getAction() == MotionEvent.ACTION_UP &&
+                    !getLocationOnScreen((EditText) innerView).contains(x, y)) {
+
+                InputMethodManager input = (InputMethodManager)
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
+                input.hideSoftInputFromWindow(getWindow().getCurrentFocus()
+                        .getWindowToken(), 0);
+            }
+        }
+
+        return handleReturn;
+    }
+
 }
